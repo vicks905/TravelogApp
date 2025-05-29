9a62add6689f703a7f2b7746ad646f60e4b12ca0
trashken0906 <kennethlim9428@gmail.com>
2025-05-29 17:15:51 +0800

    updated

--- app/src/main/java/com/ltu/m7019e/travelogapp/ui/screen/DestinationGridScreen.kt
+++ app/src/main/java/com/ltu/m7019e/travelogapp/ui/screen/DestinationGridScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
